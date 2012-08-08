cba813e979e172be53bad19eb67cf5e6c711afcd
admin-avn <admin@avn.info.ve>
2012-08-08 10:27:58 -0430

    Añadiendo un contenedor a los dos bloques izquierda y derecha para evitar que el pie de página se suba

--- sites/all/themes/avn/layouts/front/panels-front.tpl.php
+++ sites/all/themes/avn/layouts/front/panels-front.tpl.php
@@ -5,70 +5,72 @@
 		</div>
 	<?php endif ?>
 
-	<div class="contenedor-izquierdo clearfix">
-		<?php if($content['slideshow']): ?>
-			<div class="panel-slideshow">
-				<div class="inside"><?php print $content['slideshow']; ?></div>
-			</div>
-		<?php endif ?>
-		
-		<?php if($content['6-noticias']): ?>
-			<div class="panel-6-noticias">
-				<div class="inside"><?php print $content['6-noticias']; ?></div>
-			</div>
-		<?php endif ?>
-
-		<div class="contenedor clearfix">
-			<?php if($content['noticias-autogeneradas']): ?>
-				<div class="panel-noticias-autogeneradas">
-					<div class="inside"><?php print $content['noticias-autogeneradas']; ?></div>
+	<div class="contenedor clearfix">
+		<div class="contenedor-izquierdo clearfix">
+			<?php if($content['slideshow']): ?>
+				<div class="panel-slideshow">
+					<div class="inside"><?php print $content['slideshow']; ?></div>
 				</div>
 			<?php endif ?>
-			<?php if($content['notas-graficas']): ?>
-				<div class="panel-notas-graficas">
-					<div class="inside"><?php print $content['notas-graficas']; ?></div>
+			
+			<?php if($content['6-noticias']): ?>
+				<div class="panel-6-noticias">
+					<div class="inside"><?php print $content['6-noticias']; ?></div>
+				</div>
+			<?php endif ?>
+
+			<div class="contenedor clearfix">
+				<?php if($content['noticias-autogeneradas']): ?>
+					<div class="panel-noticias-autogeneradas">
+						<div class="inside"><?php print $content['noticias-autogeneradas']; ?></div>
+					</div>
+				<?php endif ?>
+				<?php if($content['notas-graficas']): ?>
+					<div class="panel-notas-graficas">
+						<div class="inside"><?php print $content['notas-graficas']; ?></div>
+					</div>
+				<?php endif ?>
+			</div>
+		</div>
+
+		<div class="contenedor-derecho clearfix">
+			<?php if($content['audiovisuales']): ?>
+				<div class="panel-audiovisuales">
+					<div class="inside"><?php print $content['audiovisuales']; ?></div>
+				</div>
+			<?php endif ?>
+
+			<?php if($content['titulares']): ?>
+				<div class="panel-titulares">
+					<div class="inside"><?php print $content['titulares']; ?></div>
+				</div>
+			<?php endif ?>
+			<?php if($content['lo-mas-leido']): ?>
+				<div class="panel-lo-mas-leido">
+					<div class="inside"><?php print $content['lo-mas-leido']; ?></div>
+				</div>
+			<?php endif ?>
+			<?php if($content['reportajes']): ?>
+				<div class="panel-reportajes">
+					<div class="inside"><?php print $content['reportajes']; ?></div>
+				</div>
+			<?php endif ?>
+			<?php if($content['opinion']): ?>
+				<div class="panel-opinion">
+					<div class="inside"><?php print $content['opinion']; ?></div>
+				</div>
+			<?php endif ?>
+			<?php if($content['debate']): ?>
+				<div class="panel-debate">
+					<div class="inside"><?php print $content['debate']; ?></div>
+				</div>
+			<?php endif ?>
+			<?php if($content['especiales']): ?>
+				<div class="panel-especiales">
+					<div class="inside"><?php print $content['especiales']; ?></div>
 				</div>
 			<?php endif ?>
 		</div>
-	</div>
-
-	<div class="contenedor-derecho clearfix">
-		<?php if($content['audiovisuales']): ?>
-			<div class="panel-audiovisuales">
-				<div class="inside"><?php print $content['audiovisuales']; ?></div>
-			</div>
-		<?php endif ?>
-
-		<?php if($content['titulares']): ?>
-			<div class="panel-titulares">
-				<div class="inside"><?php print $content['titulares']; ?></div>
-			</div>
-		<?php endif ?>
-		<?php if($content['lo-mas-leido']): ?>
-			<div class="panel-lo-mas-leido">
-				<div class="inside"><?php print $content['lo-mas-leido']; ?></div>
-			</div>
-		<?php endif ?>
-		<?php if($content['reportajes']): ?>
-			<div class="panel-reportajes">
-				<div class="inside"><?php print $content['reportajes']; ?></div>
-			</div>
-		<?php endif ?>
-		<?php if($content['opinion']): ?>
-			<div class="panel-opinion">
-				<div class="inside"><?php print $content['opinion']; ?></div>
-			</div>
-		<?php endif ?>
-		<?php if($content['debate']): ?>
-			<div class="panel-debate">
-				<div class="inside"><?php print $content['debate']; ?></div>
-			</div>
-		<?php endif ?>
-		<?php if($content['especiales']): ?>
-			<div class="panel-especiales">
-				<div class="inside"><?php print $content['especiales']; ?></div>
-			</div>
-		<?php endif ?>
 	</div>
 
 	<?php if($content['pie-de-pagina']): ?>
