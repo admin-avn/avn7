6d98ee4b3c8df46cffb05e85ae62f6d83afe6347
admin-avn <admin@avn.info.ve>
2012-08-13 14:29:47 -0430

    Añadiendo la primera plantilla de noticias autogeneradas

---
+++ sites/all/themes/avn/templates/noticias/views-view-fields--noticias--block-3.tpl.php
@@ -0,0 +1,8 @@
+<div class="noticia clearfix">
+	<?php
+	print $fields['php']->content;
+	print $fields['field_titulo_portada']->content;
+	print $fields['field_imagen']->content;
+	print $fields['field_sumario']->content;
+	?>
+</div>
