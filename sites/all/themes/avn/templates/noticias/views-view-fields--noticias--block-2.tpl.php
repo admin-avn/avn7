4c868ba38777d5a35bf3207919971a331afde65a
admin-avn <admin@avn.info.ve>
2012-08-20 14:32:23 -0430

    Realizando el corte de las imágenes

---
+++ sites/all/themes/avn/templates/noticias/views-view-fields--noticias--block-2.tpl.php
@@ -0,0 +1,14 @@
+<div class="noticia clearfix">
+	<?php
+	print $fields['php']->content;
+  if($fields['field_corte_imagen']->content == 1) {
+    $imagen = theme('image_style', array('style_name' => 'front-6-noticias-arriba', 'path' => $fields['filename']->content));
+  } elseif($fields['field_corte_imagen']->content == 2) {
+    $imagen = theme('image_style', array('style_name' => 'front-6-noticias-centro', 'path' => $fields['filename']->content));
+  } elseif($fields['field_corte_imagen']->content == 3) {
+    $imagen = theme('image_style', array('style_name' => 'front-6-noticias-abajo', 'path' => $fields['filename']->content));
+  }
+	print '<div class="imagen">' . $imagen . '</div>';
+	print $fields['field_titulo_portada']->content;
+	?>
+</div>
