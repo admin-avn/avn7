7fb8b558b587d622f3717d736ed14e3302c6dd35
admin-avn <admin@avn.info.ve>
2012-08-22 10:58:54 -0430

    Eliminando la plantilla de audiovisual ya que la vista se convirtió en un slideshow

--- sites/all/themes/avn/templates/noticias/views-view-fields--noticias--block-2.tpl.php
+++ sites/all/themes/avn/templates/noticias/views-view-fields--noticias--block-2.tpl.php
@@ -1,13 +1,13 @@
 <div class="noticia clearfix">
 	<?php
 	print $fields['php']->content;
-  if($fields['field_corte_imagen']->content == 1) {
-    $imagen = theme('image_style', array('style_name' => 'front-6-noticias-arriba', 'path' => $fields['filename']->content));
-  } elseif($fields['field_corte_imagen']->content == 2) {
-    $imagen = theme('image_style', array('style_name' => 'front-6-noticias-centro', 'path' => $fields['filename']->content));
-  } elseif($fields['field_corte_imagen']->content == 3) {
-    $imagen = theme('image_style', array('style_name' => 'front-6-noticias-abajo', 'path' => $fields['filename']->content));
-  }
+        if($fields['field_corte_imagen']->content == 1) {
+                $imagen = theme('image_style', array('style_name' => 'front-6-noticias-arriba', 'path' => $fields['filename']->content));
+        } elseif($fields['field_corte_imagen']->content == 2) {
+                $imagen = theme('image_style', array('style_name' => 'front-6-noticias-centro', 'path' => $fields['filename']->content));
+        } elseif($fields['field_corte_imagen']->content == 3) {
+                $imagen = theme('image_style', array('style_name' => 'front-6-noticias-abajo', 'path' => $fields['filename']->content));
+        }
 	print '<div class="imagen">' . $imagen . '</div>';
 	print $fields['field_titulo_portada']->content;
 	?>
